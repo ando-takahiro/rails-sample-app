52539820154c094ec34546ef933b0ecb1370aa84
ando-takahiro <takahiro.ando@gmail.com>
2015-08-15 17:41:27 +0900

    practice 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :avoid_recreate, only: [:new, :create]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -52,6 +53,9 @@ class UsersController < ApplicationController
     end
 
     # Before actions
+    def avoid_recreate
+      redirect_to(root_path) if signed_in?
+    end
 
     def signed_in_user
       unless signed_in?
